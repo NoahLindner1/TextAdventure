93840f8d8d71d1ada4bf7fe4e9b45cf91847541a
avargas17 <73358723+avargas17@users.noreply.github.com>
2021-03-04 13:12:16 -0600

    Update App.js

--- textadventure/src/App.js
+++ textadventure/src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './Components/NavBar';
 import './App.css';
+import Home from './Components/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-			Poverty text Adventure
-      </h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
-export default App;
+
