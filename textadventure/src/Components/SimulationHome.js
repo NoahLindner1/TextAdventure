285a1f0ddfdcdc827e5abfb3398c6297210a3c37
Noah Lindner <nlindner@carthage.edu>
2021-03-09 13:30:16 -0600

    Added cards to simulations home
    Added cards to simulations branch to use as a template for the peoples profiles

--- textadventure/src/Components/SimulationHome.js
+++ textadventure/src/Components/SimulationHome.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Cards from './Cards';
 
 export class SimulationHome extends Component {
 	render() {
 		return (
 			<div>
 				Profiles will go here. I am inside the Simulation Home Component rn
+				<Cards />
 			</div>
 		)
 	}
