f596d8380ce749c6ff5050ec83b0045e6ffe170c
Noah Lindner <nlindner@carthage.edu>
2021-03-09 13:22:24 -0600

    Added Simulation Home Page
    Added a simulation home page and added the route in App.js

--- textadventure/src/App.js
+++ textadventure/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './Components/NavBar';
 import './App.css';
 import Home from './Components/Home';
+import { SimulationHome } from "./Components/SimulationHome.js";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
+		  <Route path='/simulations' exact component={SimulationHome} />
         </Switch>
       </Router>
     </>
---
+++ textadventure/src/Components/SimulationHome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SimulationHome extends Component {
+	render() {
+		return (
+			<div>
+				Profiles will go here. I am inside the Simulation Home Component rn
+			</div>
+		)
+	}
+}
+
+export default SimulationHome
