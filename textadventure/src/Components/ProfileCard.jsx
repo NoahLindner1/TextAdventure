713bc32e4f9d1e99fbd037261f582aba33b1930e
Brad Dodds <braddodds1399@gmail.com>
2021-03-15 18:37:55 -0500

    Added profile cards in simulations route.

---
+++ textadventure/src/Components/ProfileCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ProfileCard = props => {
+    return(
+        <div className='card text-center'>
+            <div className='overflow'>
+                <img src= {props.imgsrc} alt='Image of woman with baby' className='card-img-top'/>
+            </div>
+            <div className='card-body text-dark'>
+                <h4 className='card-title'>{props.title}</h4>
+                <p className='card-text text-secondary'>
+                    {props.description}
+                </p>
+                <a href='#' className='btn btn-outline-success'>
+                    Simulate
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default ProfileCard;
--- textadventure/src/Components/SimulationHome.js
+++ textadventure/src/Components/SimulationHome.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import Cards from './Cards';
 import CardItem from './CardItem';
+import ProfileCard from './ProfileCards';
 
 export class SimulationHome extends Component {
 	render() {
 		return (
 			<div>
 				Profiles will go here. I am inside the Simulation Home Component rn
-				<Cards />
+				<ProfileCard />
 			</div>
 		)
 	}
