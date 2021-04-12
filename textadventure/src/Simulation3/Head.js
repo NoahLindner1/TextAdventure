c05226d91ba72cd0ee94715dcccf29c83e3e7e23
Noah Lindner <nlindner@carthage.edu>
2021-04-12 11:11:46 -0500

    fixed header
    started sim 3 template

--- textadventure/src/Simulation3/Head.js
+++ textadventure/src/Simulation3/Head.js
@@ -4,8 +4,8 @@ import {Link} from "react-router-dom";
 export class Head_3 extends Component {
 	render() {
 		return (
-			<div>
-				<h1>You are a 75-year-old woman living is subsidized housing. You've never worked but get a monthly income from your dead husband's social security benefit. You are in ill health and are on many medications that sometimes require you to choose between healthy food and having enough medicine. Your neighbor's daughter will take you to the store now and then, but is unreliable. You also use public transportation to get to your doctor's appointments when you feel well enough to get there.</h1>
+			<div className="headBody">
+				<p className="userStory1">You are a 75-year-old woman living is subsidized housing. You've never worked but get a monthly income from your dead husband's social security benefit. You are in ill health and are on many medications that sometimes require you to choose between healthy food and having enough medicine. Your neighbor's daughter will take you to the store now and then, but is unreliable. You also use public transportation to get to your doctor's appointments when you feel well enough to get there.</p>
                 <h2>CHALLENGE: Purchase this month's supply of medications.</h2>                
                 <Link to="/Simulation3/Option1">
                 <button
